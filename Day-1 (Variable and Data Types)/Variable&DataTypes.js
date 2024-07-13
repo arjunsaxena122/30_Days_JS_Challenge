650685c7a210aa5789cd32dd0248c711b45e8851
arjunsaxena122 <arjunsaxena122@gmail.com>
2024-07-13 10:29:32 +0530

    Day-1 Complete of 30_JS_Challenge

---
+++ Day-1 (Variable and Data Types)/Variable&DataTypes.js
@@ -0,0 +1,41 @@
+// Task One:
+
+var num = 7;
+console.log("Task One var Data Type value:- ",num);
+
+// Task Two:
+
+let str = "Arjun Saxena"
+console.log("Task two let Data Type value:- ",str)
+
+// Task Three:
+
+const bool = true;
+console.log("Task three boolean Data Type value:- ",bool)
+
+// Task Four:
+
+console.log("Data Type of Num variable:- ",typeof(num))
+console.log("Data Type of String variable:- ",typeof(str))
+console.log("Data Type of Boolean variable:- ",typeof(bool))
+
+const obj = {
+    name:"arjun",
+    age:21,
+}
+
+console.log("Type of the Object:- ",typeof(obj))
+
+const arr=["Mango","Orange","Apple"]
+
+console.log("Type of the Array:- ",typeof(arr))
+
+// Task Five:
+
+str = "Arjun"
+console.log("Resign the let value:- ",str)
+
+// Task six:
+
+bool = false;
+console.log("Resign the Constant Value:- ",bool)
