32be8f50a6e10e54e8b2877bef80c114d1ad9110
arjunsaxena122 <arjunsaxena122@gmail.com>
2024-08-06 13:25:06 +0530

    Completed Day-15 30_Days_JS_Challenge

--- Day-15 (Closures)/Closures.js
+++ Day-15 (Closures)/Closures.js
@@ -70,7 +70,6 @@ function createArray(...num) {
   for (let i = 0; i < num.length; i++) {
     arr.push(num[i]);
     function arrayIndex() {
-      console.log(arr);
       for (let i = 0; i < arr.length; i++) {
         console.log(`${i}`);
       }
@@ -79,11 +78,60 @@ function createArray(...num) {
   return arrayIndex();
 }
 
-console.log(createArray("arjun", "deepak", "rohan", "deepak", "aman"));
+createArray("arjun", "deepak", "rohan", "deepak", "aman");
 
 // Module Pattern:-
 
 // Task Six:-
+
+function taskSixModules() {
+  let listItem = [];
+  return function getMethods() {
+    return {
+      getAdd(item) {
+        listItem.push(item);
+        console.log("Items are added");
+        console.log(listItem);
+      },
+
+      getRemove(item) {
+        let index = listItem.indexOf(item);
+        if (index !== -1) {
+          listItem.splice(index, 1);
+          console.log("Item is removed from the list");
+        } else {
+          console.log("This item can't be exist");
+        }
+      },
+
+      getListItem() {
+        for (let i = 0; i < listItem.length; i++) {
+          console.log("List of all Items are: ", listItem[i]);
+        }
+      },
+
+      // Method second to display list items
+
+      //   listItems: function() {
+      //     if (listItem.length > 0) {
+      //         console.log("Current items:", listItem);
+      //     } else {
+      //         console.log("No items in the collection.");
+      //     }
+      // }
+    };
+  };
+}
+
+const ItemList = taskSixModules();
+ItemList().getAdd(12);
+ItemList().getAdd(13);
+ItemList().getAdd(14);
+ItemList().getAdd(15);
+ItemList().getAdd(16);
+ItemList().getAdd(17);
+ItemList().getRemove(17);
+ItemList().getListItem();
 
 // Memoization:-
 
@@ -91,21 +139,17 @@ console.log(createArray("arjun", "deepak", "rohan", "deepak", "aman"));
 
 function memoizationValue() {
   const storeValue = {};
-  
-  function prevData(num) {
+  return function prevData(num) {
     const key = JSON.stringify(num); // Create a unique key for the arguments
     if (storeValue[key]) {
-      console.log('Fetching from cache:', storeValue[key]);
+      console.log("Fetching from cache:", storeValue[key]);
       return storeValue[key];
     } else {
-      console.log('Calculating result');
-      console.log(storeValue[key] = num)
-      console.log(storeValue)
+      console.log("Calculating result");
+      storeValue[key] = num;
       return storeValue[key];
     }
-  }
-  
-  return prevData;
+  };
 }
 
 const memoizedAdd = memoizationValue();
@@ -114,4 +158,30 @@ function addNum(numOne, numTwo) {
   return numOne + numTwo;
 }
 
-console.log(memoizedAdd(addNum(3, 6))); // Fetching from cache
+console.log(memoizedAdd(addNum(3, 6)));
+console.log(memoizedAdd(addNum(3, 6)));
+console.log(memoizedAdd(addNum(4, 3)));
+console.log(memoizedAdd(addNum(4, 3)));
+console.log(memoizedAdd(addNum(12, 34)));
+console.log(memoizedAdd(addNum(12, 34)));
+console.log(memoizedAdd(addNum(12, 45)));
+console.log(memoizedAdd(addNum(12, 45)));
+
+// Task Eight:-
+
+function factNumber(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
+}
+
+const memoizedFactNumber = memoizationValue();
+console.log("Task Eight Factorial Number: ");
+console.log(memoizedFactNumber(factNumber(5)));
+console.log(memoizedFactNumber(factNumber(5)));
+console.log(memoizedFactNumber(factNumber(6)));
+console.log(memoizedFactNumber(factNumber(6)));
+console.log(memoizedFactNumber(factNumber(7)));
+console.log(memoizedFactNumber(factNumber(7)));
