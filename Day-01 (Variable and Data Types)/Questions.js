9a06d14a3216b66d017bb697ddc72af1a8192840
arjunsaxena122 <arjunsaxena122@gmail.com>
2024-08-13 09:01:04 +0530

    Completed Day-1 of 30_Days_JS_Challenge

---
+++ Day-01 (Variable and Data Types)/Questions.js
@@ -0,0 +1,30 @@
+/* Questions Day 1:-
+
+
+Task:-
+
+
+variable Declaration:
+
+1. Declare a variable using var, assign it a number, and log the value to the console.
+
+2. Declare a variable using let, assign it a number, and log the value to the console.
+
+Constant Declaration:
+
+3. Declare a variable using const, assign it a boolean value, and log the value to the console.
+
+DataTypes:
+
+4. Create variable of different data types (number,string, boolean, object, array) and log each variable's type using the typeof operator.
+
+Reassigning Variables:
+
+5. Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+
+Understanding Const:
+
+6. Try reassigning a variable declared with const and observe the error.
+
+
+*/
